559ed4b8985d77a74583e5fb5f365493498c1233
Ayush raj <rayush920@gmail.com>
2023-07-28 17:38:54 +0530

    files added

---
+++ code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+
+
+int main()
+{
+        int n,k;
+        cin>>n>>k;
+        vector<int> skills(n+5);
+        while(1)
+        {
+          cout<<1<<endl;
+        }
+        // solve(skills,0,n,k);
+
+}
